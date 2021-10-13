0c6252284b01d2a03ade6661e89a22eb05f62c64
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-10-13 23:43:14 +0500

    Work in Progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let [addTodo,setTodo]=useState([])
+  let [inpVal,setVal]=useState('')
+  let handleAdd=()=>{
+    // setTodo=([...addTodo,inpVal])
+    addTodo.push(inpVal)
+    console.log("Handle Add run")
+    console.log(inpVal)
+    console.log(addTodo)
+
+  }
+  const handleChange=(e)=>{
+    setVal(e.target.value)
+    console.log("Handle change run")
+    console.log(e.target.value)
+    
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React ToDo App </h1>
+      <input value={inpVal} onChange={handleChange} placeholder="Write your task" type="text" />
+      <button onClick={handleAdd}>Add</button><br />
+     
+        {
+          addTodo.map((val,index)=>{return <p key={index+1}>{index}:{val} <button className="btn">Delete</button><button className="btn">Update</button></p>})
+        }
+     
+        <button>Delete All</button>
     </div>
   );
 }
